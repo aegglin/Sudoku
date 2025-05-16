536bede18caec8c109c0f081e16004f8975ef14b
Aiden Egglin <uefadude@gmail.com>
2025-05-15 23:06:11 -0400

    Add finishing touches

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Sudoku {
+
+    class Tile extends JButton {
+        int r;
+        int c;
+
+        Tile(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
     public int boardWidth = 600;
     public int boardHeight = 650;
 
@@ -9,9 +23,34 @@ public class Sudoku {
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
     JPanel boardPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+    JButton numSelected = null;
+    int errors = 0;
+
+    String[] puzzle = {
+            "--74916-5",
+            "2---6-3-9",
+            "-----7-1-",
+            "-586----4",
+            "--3----9-",
+            "--62--187",
+            "9-4-7---2",
+            "67-83----",
+            "81--45---",
+    };
+    String[] solution = {
+            "387491625",
+            "241568379",
+            "569327418",
+            "758619234",
+            "123784596",
+            "496253187",
+            "934176852",
+            "675832941"
+    };
 
     public Sudoku() {
-        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,5 +65,96 @@ public class Sudoku {
 
         textPanel.add(textLabel);
         frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(9, 9));
+        setupTiles();
+        frame.add(boardPanel, BorderLayout.CENTER);
+
+        buttonsPanel.setLayout(new GridLayout(1, 9));
+        setupButtons();
+        frame.add(buttonsPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+
+    }
+
+    public void setupButtons() {
+        for (int i = 1; i < 10; i++) {
+            JButton button = new JButton();
+            button.setFont(new Font("Arial", Font.BOLD, 20));
+            button.setText(String.valueOf(i));
+            button.setFocusable(false);
+            button.setBackground(Color.WHITE);
+            buttonsPanel.add(button);
+
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JButton button = (JButton) e.getSource();
+                    if (numSelected != null) {
+                        numSelected.setBackground(Color.WHITE);
+                    }
+                    numSelected = button;
+                    numSelected.setBackground(Color.LIGHT_GRAY);
+                }
+            });
+        }
+    }
+
+    public void setupTiles() {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                Tile tile = new Tile(r, c);
+                char tileChar = puzzle[r].charAt(c);
+                if (tileChar != '-') {
+                    tile.setFont(new Font("Arial", Font.BOLD, 20));
+                    tile.setBackground(Color.lightGray);
+                    tile.setText(String.valueOf(tileChar));
+                }
+                else {
+                    tile.setFont(new Font("Arial", Font.PLAIN, 20));
+                    tile.setBackground(Color.WHITE);
+                }
+
+                if ((r == 2 && c == 2) || (r == 2 && c == 5) || (r == 5 && c == 2) || (r == 5 && c == 5)) {
+                    tile.setBorder(BorderFactory.createMatteBorder(1, 1, 5, 5, Color.BLACK));
+                }
+                else if (r == 2 || r == 5) {
+                    tile.setBorder(BorderFactory.createMatteBorder(1, 1, 5, 1, Color.BLACK));
+                }
+                else if (c == 2 || c == 5) {
+                    tile.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 5, Color.BLACK));
+                }
+                else {
+                    tile.setBorder(BorderFactory.createLineBorder(Color.BLACK)); //don't need thickness unlike matte border
+                }
+                tile.setFocusable(false);
+                boardPanel.add(tile);
+
+                tile.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        Tile tile = (Tile)e.getSource();
+                        int r = tile.r;
+                        int c = tile.c;
+
+                        if (numSelected != null) {
+                            if (!tile.getText().equals("")) {
+                                return;
+                            }
+                            String numSelectedText = numSelected.getText();
+                            String tileSolution = String.valueOf(solution[r].charAt(c));
+
+                            if (tileSolution.equals(numSelectedText)) {
+                                tile.setText(numSelectedText);
+                                errors--;
+                            }
+                            else {
+                                errors++;
+                                textLabel.setText("Sudoku Errors: " + String.valueOf(errors));
+                            }
+                        }
+                    }
+                });
+            }
+        }
     }
 }
